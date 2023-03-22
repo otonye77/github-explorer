d375609e020db3a4d1dbbebedce0765f00379e09
otonye77 <otonye.amietubodie@decagon.dev>
2023-03-22 16:02:58 +0100

    implemented toggle

--- src/component/searchbar/searchbar.component.jsx
+++ src/component/searchbar/searchbar.component.jsx
@@ -1,29 +1,41 @@
 import { useState } from "react";
 import TextField from "@mui/material/TextField";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import axios from "axios";
 import "./searchbar.css";
 
 const BasicTextFields = () => {
   const [query, setQuery] = useState("");
   const [users, setUsers] = useState([]);
+  const [arrow, setArrow] = useState(false);
 
   const handleChange = (e) => {
     setQuery(e.target.value);
   };
-  console.log(query);
+
   const getUsers = async (e) => {
     e.preventDefault();
     let response = await axios.get(
       `https://api.github.com/search/users?q=${query}&per_page=5`
     );
     setUsers(response.data.items);
-    console.log(response);
   };
-  console.log(users);
+
+  const handleArrowClick = () => {
+    setArrow(!arrow)
+  }
 
   const getRepos = async (username) => {
-    let res = await axios.get(`https://api.github.com/users/${username}/repos`)
-    console.log(res);
+    let res = await axios.get(`https://api.github.com/users/${username}/repos`);
+    let userCopy = [...users];
+    let selectedUser = userCopy.find((user) => user.login === username);
+    selectedUser.repos = res.data.map((repo) => ({
+      name: repo.name,
+      description: repo.description,
+      stars: repo.stargazers_count,
+    }));
+    setUsers(userCopy);
   };
 
   return (
@@ -41,10 +53,29 @@ const BasicTextFields = () => {
         </form>
       </div>
       <div className="list">
+        <h3 className="header">{`Showing users for "${query}"`}</h3>
+        <br />
         {users.map((user) => (
-          <h1 onClick={() => getRepos(user.login)} className="name" key={user.id}>
-            {user.login}
-          </h1>
+          <div key={user.id}>
+            <div className="card-container">
+                <h1 className="name">{user.login}</h1>
+                 <ExpandMoreIcon   onClick={() => getRepos(user.login)} className="expand-more" />
+            </div>
+            <ul>
+              {user.repos &&
+                user.repos.map((repo) => (
+                  <div className="repo-card" key={repo.name}>
+                    <h4 className="reponame">{repo.name}</h4>
+                    <p className="repodescription">{repo.description}</p>
+                    {/* <div className="rating">
+                  {Array.from({ length: repo.stars }).map((_, index) => (
+                    <span key={index}>★</span>
+                  ))}
+                </div> */}
+                  </div>
+                ))}
+            </ul>
+          </div>
         ))}
       </div>
     </div>
