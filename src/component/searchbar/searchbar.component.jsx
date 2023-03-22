d0e96f0668fff6cbf31f58ffaa2b34dd8286511f
otonye77 <otonye.amietubodie@decagon.dev>
2023-03-22 14:21:35 +0100

    worked on the getUser function and getRepo function

--- src/component/searchbar/searchbar.component.jsx
+++ src/component/searchbar/searchbar.component.jsx
@@ -5,6 +5,7 @@ import "./searchbar.css";
 
 const BasicTextFields = () => {
   const [query, setQuery] = useState("");
+  const [users, setUsers] = useState([]);
 
   const handleChange = (e) => {
     setQuery(e.target.value);
@@ -12,27 +13,42 @@ const BasicTextFields = () => {
   console.log(query);
   const getUsers = async (e) => {
     e.preventDefault();
-    let response = await axios.get(`https://api.github.com`)
+    let response = await axios.get(
+      `https://api.github.com/search/users?q=${query}&per_page=5`
+    );
+    setUsers(response.data.items);
     console.log(response);
-  }
-  
+  };
+  console.log(users);
+
+  const getRepos = async (username) => {
+    let res = await axios.get(`https://api.github.com/users/${username}/repos`)
+    console.log(res);
+  };
+
   return (
-    <div className="container">
-      <form className="form-container">
-        <TextField
-          onChange={handleChange}
-          id="outlined-basic"
-          className="outlined"
-          label="Enter Username"
-          variant="outlined"
-        />
-        <button onClick={getUsers}>Search</button>
-      </form>
+    <div className="overall">
+      <div className="container">
+        <form className="form-container">
+          <TextField
+            onChange={handleChange}
+            id="outlined-basic"
+            className="outlined"
+            label="Enter Username"
+            variant="outlined"
+          />
+          <button onClick={getUsers}>Search</button>
+        </form>
+      </div>
+      <div className="list">
+        {users.map((user) => (
+          <h1 onClick={() => getRepos(user.login)} className="name" key={user.id}>
+            {user.login}
+          </h1>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default BasicTextFields;
-
-
-
